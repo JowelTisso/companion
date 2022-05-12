a6f87aa125d2f34dc15568405e2f1ae5698c0b00
Jowel Tisso <52632590+JowelTisso@users.noreply.github.com>
2022-05-12 21:49:38 +0530

    fix: netlify build issue fix

--- src/pages/auth/login/Login.jsx
+++ src/pages/auth/login/Login.jsx
@@ -1,5 +1,5 @@
 import "./Login.css";
-import React, { useState } from "react";
+import React from "react";
 import logo from "../../../assets/logo.png";
 import { TextField, Button } from "@mui/material";
 
