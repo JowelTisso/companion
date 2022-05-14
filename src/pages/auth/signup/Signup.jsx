1ccf8ad2ec3255112482114cdb91d96142eead18
Jowel Tisso <52632590+JowelTisso@users.noreply.github.com>
2022-05-14 14:43:02 +0530

    feat: more fields added in signup

--- src/pages/auth/signup/Signup.jsx
+++ src/pages/auth/signup/Signup.jsx
@@ -1,40 +1,56 @@
+import "./Signup.css";
 import React, { useState } from "react";
 import logo from "../../../assets/logo.png";
 import { TextField, Button } from "@mui/material";
-import { userLogIn, userSignUp } from "../helper/authHelper";
-import { useDispatch } from "react-redux";
-import { useLocation, useNavigate } from "react-router-dom";
-import { logIn } from "../../../store/authSlice";
+import { userSignUp } from "../helper/authHelper";
+import { useNavigate } from "react-router-dom";
 import { callToast } from "../../../components/toast/Toast";
 import { ROUTES } from "../../../utils/Constant";
 
 const Signup = () => {
   const defaultCredential = {
+    fullname: "",
     username: "",
+    email: "",
     password: "",
+    confirmPassword: "",
   };
   const [credentials, setCredentials] = useState(defaultCredential);
 
-  const { username, password } = credentials;
-  //   const dispatch = useDispatch();
+  const { username, password, fullname, email, confirmPassword } = credentials;
   const navigate = useNavigate();
 
+  const fullnameChangeHandler = ({ target }) => {
+    setCredentials((state) => ({ ...state, fullname: target.value }));
+  };
   const usernameChangeHandler = ({ target }) => {
     setCredentials((state) => ({ ...state, username: target.value }));
   };
   const passwordChangeHandler = ({ target }) => {
     setCredentials((state) => ({ ...state, password: target.value }));
   };
+  const emailChangeHandler = ({ target }) => {
+    setCredentials((state) => ({ ...state, email: target.value }));
+  };
+  const confirmPasswordChangeHandler = ({ target }) => {
+    setCredentials((state) => ({ ...state, confirmPassword: target.value }));
+  };
 
   const signupHandler = async () => {
     try {
-      if (username && password) {
-        const res = await userSignUp({
-          username: username,
-          password: password,
-        });
-        if (res?.status === 200 || res?.status === 201) {
-          setCredentials(defaultCredential);
+      if (username && password && email && fullname) {
+        if (password === confirmPassword) {
+          const res = await userSignUp({
+            username,
+            password,
+            fullname,
+            email,
+          });
+          if (res?.status === 200 || res?.status === 201) {
+            setCredentials(defaultCredential);
+          }
+        } else {
+          callToast("Password doesn't match!", false);
         }
       } else {
         callToast("All fields are required!", false);
@@ -49,8 +65,14 @@ const Signup = () => {
       <section className="logo-section">
         <img className="logo" src={logo} alt="logo" />
       </section>
-      <section className="login-section">
+      <section className="login-section signup-section">
         <p className="login-heading t2 text-center">Become a companion</p>
+        <TextField
+          label="Fullname"
+          variant="outlined"
+          value={fullname}
+          onChange={fullnameChangeHandler}
+        />
         <TextField
           label="Username"
           variant="outlined"
@@ -58,11 +80,27 @@ const Signup = () => {
           onChange={usernameChangeHandler}
         />
         <TextField
-          label="Password"
+          label="Email"
           variant="outlined"
-          value={password}
-          onChange={passwordChangeHandler}
+          value={email}
+          onChange={emailChangeHandler}
         />
+        <section className="password-container">
+          <TextField
+            className="password-field"
+            label="Password"
+            variant="outlined"
+            value={password}
+            onChange={passwordChangeHandler}
+          />
+          <TextField
+            className="password-field"
+            label="Confirm password"
+            variant="outlined"
+            value={confirmPassword}
+            onChange={confirmPasswordChangeHandler}
+          />
+        </section>
         <Button
           variant="contained"
           size={"large"}
@@ -72,12 +110,7 @@ const Signup = () => {
           Sign up
         </Button>
         <p className="t4 text-center">OR</p>
-
-        <div className="secondary-txt-container">
-          <p className="t5 text-center txt-secondary">
-            Already have an account!
-          </p>
-        </div>
+        <p className="t5 text-center txt-secondary">Already have an account!</p>
         <Button
           variant="contained"
           size={"large"}
