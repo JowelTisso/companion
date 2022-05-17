89e9e59341bb327a33e9a18abc3c911895e463fb
Jowel Tisso <52632590+JowelTisso@users.noreply.github.com>
2022-05-17 13:59:52 +0530

    refactor: removed data from db

--- src/backend/db/users.js
+++ src/backend/db/users.js
@@ -12,35 +12,7 @@ export const users = [
     lastName: "tisso",
     username: "joweltisso",
     password: "test123",
-    bookmarks: [
-      {
-        _id: uuid(),
-        content: "Roadmap to become a full stack web3 developer in 180 days.",
-        images: ["https://picsum.photos/id/1/800/600"],
-        likes: {
-          likeCount: 0,
-          likedBy: [],
-          dislikedBy: [],
-        },
-        username: "joweltisso",
-        createdAt: formatDate(),
-        updatedAt: formatDate(),
-      },
-      {
-        _id: uuid(),
-        content:
-          "Love the idea behind this. A clear goal and how to achieve it. I believe as you go, you would realize that you need more time with some. And sometimes, I may intend to spend selected hours learning but end up spending more. Wish you the best!👍🏾",
-        images: null,
-        likes: {
-          likeCount: 0,
-          likedBy: [],
-          dislikedBy: [],
-        },
-        username: "shubhamsoni",
-        createdAt: formatDate(),
-        updatedAt: formatDate(),
-      },
-    ],
+    bookmarks: [],
     createdAt: formatDate(),
     updatedAt: formatDate(),
   },
